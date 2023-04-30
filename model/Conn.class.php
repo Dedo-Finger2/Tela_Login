6ea97b53a6c989cf53adc9d41be019a572ed716e
Dedo_Finger2 <antonioimportant@gmail.com>
2023-04-30 09:02:48 -0300

    add conexao

--- model/Conn.class.php
+++ model/Conn.class.php
@@ -1,3 +1,22 @@
 <?php
+// classe abstrata com a conexão com o banco de dados
 
-    // classe abstrata com a conexão com o banco de dados
+use Exception;
+
+class Conexao
+{
+    protected static $conexao;
+
+    public static function conectar()
+    {
+        if (!isset(self::$conexao)) {
+            try {
+                self::$conexao = new mysqli("localhost", "root", "", "user_list");
+            } catch (Exception $e) {
+                return $e;
+            }
+            self::$conexao->set_charset("utf8");
+        }
+        return self::$conexao;
+    }
+}
